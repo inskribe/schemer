0f133c1481d14b2d42c6456aac9b2b4da2b56772
inskribe <inskribe.dev@tuta.com>
2025-12-12 22:49:27 -0500

    fix(logging): format error chain

--- internal/errschemer/error.go
+++ internal/errschemer/error.go
@@ -1,6 +1,10 @@
 package errschemer
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type SchemerErr struct {
 	Code    string
@@ -17,4 +21,31 @@ func (e *SchemerErr) Error() string {
 
 func (e *SchemerErr) Unwrap() error {
 	return e.Err
+}
+
+func FormatChain(err error) string {
+	if err == nil {
+		return ""
+	}
+
+	var b strings.Builder
+	i := 0
+
+	for err != nil {
+		// Special-case your error type for richer output
+		if se, ok := err.(*SchemerErr); ok {
+			if se.Err != nil {
+				fmt.Fprintf(&b, "%d) SchemerErr code=%s msg=%q inner=%v\n", i, se.Code, se.Message, se.Err)
+			} else {
+				fmt.Fprintf(&b, "%d) SchemerErr code=%s msg=%q\n", i, se.Code, se.Message)
+			}
+		} else {
+			fmt.Fprintf(&b, "%d) %T: %v\n", i, err, err)
+		}
+
+		err = errors.Unwrap(err)
+		i++
+	}
+
+	return b.String()
 }
