c0b526c3f019cd4b56b05b406300e8043e8d43f6
inskribe <inskribe.dev@tuta.com>
2025-12-12 22:24:13 -0500

    fix(logging): unwrapping error chains.

--- internal/errschemer/error.go
+++ internal/errschemer/error.go
@@ -8,10 +8,13 @@ type SchemerErr struct {
 	Err     error
 }
 
-func (err *SchemerErr) Error() string {
-	return fmt.Sprintf("Error %s: %s", err.Code, err.Message)
+func (e *SchemerErr) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("Error %s: %s: %v", e.Code, e.Message, e.Err)
+	}
+	return fmt.Sprintf("Error %s: %s", e.Code, e.Message)
 }
 
-func (err *SchemerErr) Unwrap() error {
-	return err.Err
+func (e *SchemerErr) Unwrap() error {
+	return e.Err
 }
